ec913a319c3a001320ce336fc01ac7ed939cb276
Hreimur Logi <70168436+Accent8@users.noreply.github.com>
2026-09-01 17:39:51 +0000

    update

--- assignment1.py
+++ assignment1.py
@@ -3,6 +3,13 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 tit = pd.read_csv("titanic.csv")
 
-sns.histplot(data=tit, x="age", hue="alive", bins=30)
-plt.title("Age distribution by survival")
+'''sns.barplot(data=tit, x="who", y="survived", order=["man", "woman", "child"])
+plt.title("Survival Rate by Category")
+plt.ylabel("Survival Rate")
+plt.show()
+'''
+sns.barplot(data=tit, x="who", y="survived", hue="pclass",
+            order=["man", "woman", "child"])
+plt.title("Survival Rate by Category and Class")
+plt.ylabel("Survival Rate")
 plt.show()
